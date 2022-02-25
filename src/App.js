20a8cca590fd7d905d46fa33a862f4863854b512
eielelai <allison16.feliciano@gmail.com>
2022-02-25 12:17:57 +0800

    fourth

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import ReactJsTyping from "reactjs-typing-effect";
 import "./App.css";
 import { Carousel } from "react-responsive-carousel";
